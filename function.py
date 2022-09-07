73dd07e12532f54f3aac1d839d5c4978afc9dec6
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-09-07 14:07:19 +0500

    read and write data from file and functions

---
+++ function.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  7 13:55:27 2022
+
+@author: Hammad Hassan
+"""
+#def display(arr):
+#    for i in arr:
+#        print(i)
+#array=[1,2,3,4,5,6,7,8,9]
+#display(array)
+
+def get_name():
+    names=['Ali','Ahmad','Hassan']
+    return names;
+names=get_name();
+for i in range(len(names)):    
+    print(names[i])
