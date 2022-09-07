9632beec36b9d713952bb58285b3bd98da8de709
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-09-07 15:10:55 +0500

    calculating power function through recursion

--- recursion.py
+++ recursion.py
@@ -21,4 +21,18 @@ def printArray(arr,start,end):
 
 arr=[1,2,3,4,5,6,7,8,9,10]
 printArray(arr, 0, len(arr)-1)
-
+# calculating power function through recursion
+def power(n,k):
+    if(k==1):
+        return n
+    else:
+        return n*power(n,k-1)
+result=power(2, 5)
+print(result)
+# other
+num=2
+power=5
+result=1
+for i in range(power):
+    result*=num
+print(result)
