30d128c3684736eac5046f2b2a5963b3bae49c95
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-09-09 14:22:39 +0500

    Find the smallest element and place at left side for all list

---
+++ Problems/Prob4.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 07:27:51 2022
+
+@author: Hammad Hassan
+"""
+import funcs
+X=[100,1,-3,1,4,-4,0,101,35,-5]
+print(funcs.Sort4(X))
+    
--- Problems/funcs.py
+++ Problems/funcs.py
@@ -27,3 +27,17 @@ def Minimum(Arr,starting,ending):
             minNum=Arr[starting]
             index=starting;
     return index
+#Prob4
+def Sort4(Arr):
+    minIndex=Minimum(Arr,0,len(Arr)-1)
+    minValue=Arr[minIndex]
+    temp=Arr[0]
+    Arr[0]=minValue
+    Arr[minIndex]=temp
+    for i in range (len(Arr)):
+        for j in range(i + 1, len(Arr)):
+            if(Arr[i] > Arr[j]):
+                temp = Arr[i]
+                Arr[i] = Arr[j]
+                Arr[j] = temp
+    return Arr
