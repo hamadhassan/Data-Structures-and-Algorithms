64bbde41f0d788afdd8fd7539d29ad1b58e83135
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-09-09 12:27:56 +0500

    search the element

---
+++ Problems/Prob1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 06:00:11 2022
+
+@author: Hammad Hassan
+"""
+import funcs
+X=[22,2,1,7,11,13,5,2,9]
+print("Index:",funcs.SearchA(X, 2))
---
+++ Problems/funcs.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 5:45:15 2022
+
+@author: Hammad Hassan
+"""
+#Prob1
+def SearchA(Arr,x):
+    searchArr=[]
+    for i in range(len(Arr)):
+        if(x==Arr[i]):
+            searchArr.append(i)
+    return searchArr
