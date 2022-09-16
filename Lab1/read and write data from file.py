5d35bcf14d8009aa1db8ec53dfeafa7953f5299e
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-09-16 20:32:20 +0500

    calculate the total time of algorithms

--- Lab1/read and write data from file.py
+++ Lab1/read and write data from file.py
@@ -5,16 +5,15 @@ Created on Wed Sep  7 13:19:37 2022
 @author: Hammad Hassan
 """
 # Read Data from File
-given_file=open(file='test.txt',mode='r')
-lines=given_file.read()
-numbers=[]
-arr=lines.split()
-for s in arr:
-    num=int(s)
-    numbers.append(num)
-print(numbers)
-
-#arr=['Hello World','UET']
-#f=open(file="test.txt",mode="w")
-#for i in arr:
-#    f.write(i+"\n")
+#given_file=open(file='test.txt',mode='r')
+#lines=given_file.read()
+#numbers=[]
+#arr=lines.split()
+#for s in arr:
+#    num=int(s)
+#    numbers.append(num)
+#print(numbers)
+arr=[1.222222,33333.333333333,4.4]
+f=open(file="test.txt",mode="w")
+for i in arr:
+   f.write(f"{i}\n")
---
+++ Lab2/Problems/Prob7.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 16 19:49:46 2022
+
+@author: Hammad Hassan
+"""
+
+given_file=open(file='Nvalues.txt',mode='r')
+lines=given_file.read()
+values=[]
+arr=lines.split()
+for s in arr:
+    num=int(s)
+    values.append(num)
+import funcs as f
+import time as t
+ins=[]
+m=[]
+h=[]
+s=[]
+b=[]
+#Inseration Sort 
+for i in range(len(values)):
+    #Check the time for process 
+    start_time=t.time() #Time Start
+    randomArray=f.RandomArray(values[i])
+    f.InsertionSort(randomArray,0,len(randomArray))
+    end_time=t.time() #Time End
+    run_time=end_time-start_time #Calculate run time
+    ins.append(run_time)
+    print("Insertion Sort",ins)
+#Merge Sort
+for i in range(len(values)):
+    #Check the time for process 
+    start_time=t.time() #Time Start
+    randomArray=f.RandomArray(values[i])
+    f.MergeSort(randomArray,0,len(randomArray)-1)
+    end_time=t.time() #Time End
+    run_time=end_time-start_time #Calculate run time
+    m.append(run_time)
+    print("Merge Sort",m)
+#Hybrid Merge sort
+for i in range(len(values)):
+    #Check the time for process 
+    start_time=t.time() #Time Start
+    randomArray=f.RandomArray(values[i])
+    f.HybrideMergeSort(randomArray,0,len(randomArray)-1)
+    end_time=t.time() #Time End
+    run_time=end_time-start_time #Calculate run time
+    h.append(run_time)
+    print("Hybrid Merge sort",h)
+#Selection Sort 
+for i in range(len(values)):
+    #Check the time for process 
+    start_time=t.time() #Time Start
+    randomArray=f.RandomArray(values[i])
+    sortedArray=f.SelectionSort(randomArray, 0,len(randomArray))
+    end_time=t.time() #Time End
+    run_time=end_time-start_time #Calculate run time
+    s.append(run_time)
+    print("Selection Sort ",s)
+#Bubble Sort 
+for i in range(len(values)):
+    #Check the time for process 
+    start_time=t.time() #Time Start
+    randomArray=f.RandomArray(values[i])
+    sortedArray=f.BubbleSort(randomArray, 0,len(randomArray))
+    end_time=t.time() #Time End
+    run_time=end_time-start_time #Calculate run time
+    b.append(run_time)
+    print("Bubble Sort ",b)
+
+
+f=open(file="RunTime.csv",mode="w")
+for (i,m,h,s,b) in zip(ins,m,h,s,b):
+    print("I=",i,"M=",m,"H=",h,"S=",s,"B=",b)
+    f.write(f"{ins},{m},{h},{s},{b}\n")
