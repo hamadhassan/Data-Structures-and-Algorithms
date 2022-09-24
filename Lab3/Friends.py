54d3c1827dd104efb9e83b0ece3f68e67cbf3255
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-09-24 13:13:41 +0500

    Social Friends Problem

--- Lab3/Friends.py
+++ Lab3/Friends.py
@@ -4,13 +4,7 @@ Created on Sat Sep 24 06:58:45 2022
 
 @author: Hammad Hassan
 """
-#(b)
-def friendFaster(arr):
-    result=[]
-    
-    return result
-
-
+A=[[1,4],[2,5],[7,9],[9,10],[6,10]]
 #(a)
 def friendSlower(arr):
     result=[]
@@ -21,6 +15,16 @@ def friendSlower(arr):
             elif(arr[i][1]==arr[j][0]):
                 result.append(tuple((i+1,j+1)))  
     return result
-A=[[1,4],[2,5],[7,9],[9,10],[6,10]]
 print(friendSlower(A))
+#(b)
+def friendFaster(arr):
+    result=[]
+    for i in range(len(arr)-1):
+        if(arr[i+1][0]<arr[i][1]):
+            result.append(tuple((i+1,i+2)))
+        elif(arr[i][1]==arr[i+1][0]):
+            result.append(tuple((i+1,i+2)))  
+        elif(arr[i+1][0]>arr[i][1]):
+             result.append(tuple((i+2,i+3)))
+    return result
 print(friendFaster(A))
