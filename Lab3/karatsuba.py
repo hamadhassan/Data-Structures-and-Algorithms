c94c333dea6ad5501d77bc9ac92a9685b2eb84ce
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-09-24 12:46:32 +0500

    Meet Multiplication part 4

--- Lab3/karatsuba.py
+++ Lab3/karatsuba.py
@@ -132,7 +132,44 @@ def Visualize_Karatsuba(a,b):
     print(sum)                        
 Visualize_Karatsuba(22,45)
        
-
+#(iv)
+arr1=[]
+arr2=[]
+sum=[]
+def Multiply_Recursive(a,b):
+    if(a<10 or b<10):
+        arr1.append(a%10)
+        arr2.append(b%10)
+    else:
+        arr1.append(a%10)
+        a=a//10
+        arr2.append(b%10)
+        b=b//10
+        return(Multiply_Recursive(a,b))
+    carry=0
+    toadd1=""
+    for i in range(len(arr2)):
+        for j in range(len(arr1)):
+            mul=arr2[i]*arr1[j]
+            if(mul>=10):
+              carry=mul//10
+              if(carry>=1 and j<len(arr2)-1):
+                  mul+=carry
+                  toadd=str(mul%10)
+              else:
+                  toadd+=str(mul)
+                  sum.append(toadd) 
+                  
+            else:
+                if(j<len(arr2)-1):
+                    toadd1+=str(mul)+"0"                 
+                else:
+                    sum.append(str(mul)+toadd1)
+    total=0
+    for i in range(len(sum)):
+        total=total+int(sum[i])
+    return total
+print(Multiply_Recursive(22,45))
  
  
 
