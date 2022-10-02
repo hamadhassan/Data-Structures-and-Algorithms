dfc278b2e37876035a313d18d31bb2510ba83733
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-10-02 12:18:18 +0500

    Lab 4 completed

--- Lab4/Part2.py
+++ Lab4/Part2.py
@@ -72,4 +72,3 @@ for i in range(1,len(dfS)):
 
 
 
-
--- Lab4/Part4.py
+++ Lab4/Part4.py
@@ -23,3 +23,31 @@ print(code)
 # print(code)
 # df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings})
 # df.to_csv('products.csv', index=False, encoding='utf-8')
+##------------------------ Scrap the Content --------------------------------- ##
+
+##------------------------ Decelerar the data variables --------------------------------- ##
+# products=[] 
+# ratings=[]
+# prices=[] 
+##------------------------ Extract the required data --------------------------------- ##
+# for a in soup.findAll('div',attrs={'class':'_37K3-p'}):
+#     name=a.find('a', attrs={'class':'s1Q9rs'})
+#     price=a.find('div',attrs={'class':'_30jeq3'})
+#     rating=a.find('div',attrs={'class':'_3LWZlK'})
+#     products.append(name.text)
+#     prices.append(price.text)
+#     ratings.append(rating.text)
+##------------------------ Store in file --------------------------------- ##
+# df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings})
+# df.to_csv('products.csv', index=False, encoding='utf-8')
+
+##------------------------ Scrap the Content --------------------------------- ##
+
+# driver = webdriver.Chrome()
+# driver.get('https://www.youtube.com/channel/UC7RWnITwDurFDiXLbl51D0A/videos')
+# content=driver.page_source
+# soup = BeautifulSoup(content, 'lxml')
+# ##------------------------ Find desire content through soup  ------------------ ##
+# title=soup.find(class_='yt-simple-endpoint style-scope ytd-grid-video-renderer')
+# print(title).split.text()
+# driver.quit()
